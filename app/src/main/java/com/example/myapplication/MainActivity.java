f9ad4b8821582eed8496b3646abe6ecd5bb36e7f
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-06-09 22:54:13 +0700

    uhhh

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,28 @@ private Button meep,maad;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    private void What() {
+        maad = findViewById(R.id.mad);
+        meep = findViewById(R.id.Deppsito);
+    }
+    private void pushher() {
+        meep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,draw.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void pushme() {
+        meep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,with.class);
+                startActivity(intent);
+            }
+        });
     }
 }
