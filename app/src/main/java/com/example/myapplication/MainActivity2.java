48dae9e36d4baf9c6500acf409d86d945ed55326
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-06-09 15:13:43 +0700

    uuui

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,14 +1,22 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
+    private RecyclerView mRecycleView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutmanager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ArrayList<ExampleItem> examplelist = new ArrayList<>();
+        examplelist.add(new ExampleItem(R.drawable.ic_android,"Line 1", "Line 2"));
     }
 }
